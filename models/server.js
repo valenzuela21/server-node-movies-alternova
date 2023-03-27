c76dbce756b09839abb2e56f2d6250131d3eb31a
valenzuela21 <vlzdavid12@outlook.com>
2023-03-27 08:44:19 -0500

    Update

--- models/server.js
+++ models/server.js
@@ -6,7 +6,7 @@ const {socketController} = require('../controllers/sockets/controller');
 class Server {
     constructor() {
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
 
         this.server = require('http').createServer(this.app);
         this.io = require('socket.io')(this.server,{
